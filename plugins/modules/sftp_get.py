01e304c93b76b6108ab238d7d373f9cd366bc840
dvillafana <david.villafana@capcu.org>
2024-09-05 14:09:13 -0500

    (refac) reduced cyclomatic complexity

--- plugins/modules/sftp_get.py
+++ plugins/modules/sftp_get.py
@@ -3,21 +3,24 @@
 # Copyright: (c) 2024, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 import os
 import hashlib
 from ansible.module_utils._text import to_native
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 import fnmatch
+from typing import List, Dict, Any, Union
+
 try:
     import paramiko
+
     HAS_PARAMIKO = True
 except ImportError:
     HAS_PARAMIKO = False
 
 __metaclass__ = type
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: sftp_get
 author:
@@ -73,7 +76,7 @@ options:
     required: False
     type: list
     elements: str
-'''
+"""
 
 EXAMPLES = r"""
 - name: Retrieve a single file via SFTP
@@ -115,7 +118,7 @@ downloaded_files:
 """
 
 
-def get_file_hash(file_obj):
+def get_file_hash(file_obj: Any) -> str:
     """Calculate MD5 hash of file object."""
     hash_md5 = hashlib.md5()
     for chunk in iter(lambda: file_obj.read(4096), b""):
@@ -123,111 +126,135 @@ def get_file_hash(file_obj):
     return hash_md5.hexdigest()
 
 
-def main():
-    spec = dict(
-        host=dict(type='str', required=True),
-        port=dict(default=22, type='int'),
-        username=dict(type='str', required=True),
-        password=dict(type='str', required=True, no_log=True),
-        remote_path=dict(type='str', required=True),
-        local_path=dict(type='str', required=True),
-        host_key_algorithms=dict(type='list', elements='str', required=False)
-    )
+def get_connect_params(module: AnsibleModule) -> Dict[str, Any]:
+    """Get connection parameters for SSH client."""
+    params = {
+        "hostname": module.params["host"],
+        "username": module.params["username"],
+        "password": module.params["password"],
+        "port": module.params["port"],
+    }
+    if module.params["host_key_algorithms"]:
+        params["server_host_key_algorithms"] = module.params["host_key_algorithms"]
+    return params
 
-    module = AnsibleModule(
-        argument_spec=spec,
-        supports_check_mode=True
-    )
 
+def get_remote_files(
+    sftp: paramiko.SFTPClient, remote_path: str
+) -> Union[List[str], str]:
+    """Get list of remote files based on the given path."""
+    if any(char in remote_path for char in ["*", "?", "]", "["]):
+        glob_expression = os.path.basename(remote_path)
+        remote_dir = os.path.dirname(remote_path)
+        all_files = sftp.listdir(remote_dir)
+        return fnmatch.filter(all_files, glob_expression)
+    elif remote_path.endswith("/"):
+        return sftp.listdir(remote_path)
+    else:
+        return [os.path.basename(remote_path)]
+
+
+def validate_paths(module: AnsibleModule, remote_files: List[str]) -> None:
+    """Validate remote and local paths."""
+    if not remote_files:
+        module.fail_json(msg=f"No files found matching {module.params['remote_path']}")
+
+    if (
+        module.params["remote_path"].endswith("/") or len(remote_files) > 1
+    ) and not module.params["local_path"].endswith("/"):
+        module.fail_json(
+            msg=f"invalid local_path: {module.params['local_path']} -- local_path must be a directory string ending with '/' when multiple files would be retrieved"
+        )
+
+
+def download_file(
+    sftp: paramiko.SFTPClient, remote_file: str, local_file: str, remote_path: str
+) -> bool:
+    """Download a single file if it doesn't exist or has different content."""
+    if os.path.exists(local_file):
+        with open(local_file, "rb") as f:
+            local_hash = get_file_hash(f)
+        with sftp.file(os.path.join(remote_path, remote_file), "rb") as f:
+            remote_hash = get_file_hash(f)
+        if local_hash == remote_hash:
+            return False
+
+    sftp.get(os.path.join(remote_path, remote_file), local_file)
+    return True
+
+
+def process_files(
+    module: AnsibleModule, sftp: paramiko.SFTPClient, remote_files: List[str]
+) -> Dict[str, Any]:
+    """Process and download files."""
+    result = {"changed": False, "downloaded_files": []}
+    remote_path = module.params["remote_path"]
+    local_path = module.params["local_path"]
+
+    for remote_file in remote_files:
+        local_file = (
+            os.path.join(local_path, remote_file)
+            if local_path.endswith("/")
+            else local_path
+        )
+        if download_file(
+            sftp,
+            remote_file,
+            local_file,
+            os.path.dirname(remote_path) if type(remote_files) is list else remote_path,
+        ):
+            result["changed"] = True
+            result["downloaded_files"].append(local_file)
+
+    result["msg"] = (
+        f"{len(result['downloaded_files'])} file(s) downloaded successfully"
+        if result["changed"]
+        else "All files already exist at destination with the same content"
+    )
+    return result
+
+
+def run_module(module: AnsibleModule) -> None:
+    """Main function to run the Ansible module."""
     if not HAS_PARAMIKO:
         module.fail_json(msg=missing_required_lib("paramiko"))
-
-    result = {'changed': False, 'downloaded_files': []}
-
-    if module.check_mode:
-        result["msg"] = "Check mode not supported for file retrieval"
-        module.exit_json(**result)
 
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(**get_connect_params(module))
 
-        connect_params = {
-            'hostname': module.params['host'],
-            'username': module.params['username'],
-            'password': module.params['password'],
-            'port': module.params['port']
-        }
+        with ssh.open_sftp() as sftp:
+            remote_files = get_remote_files(sftp, module.params["remote_path"])
+            validate_paths(module, remote_files)
+            result = process_files(module, sftp, remote_files)
 
-        local_path: str = module.params['local_path']
-        remote_path: str = module.params['remote_path']
-
-        if module.params['host_key_algorithms']:
-            connect_params['server_host_key_algorithms'] = module.params['host_key_algorithms']
-
-        ssh.connect(**connect_params)
-
-        sftp = ssh.open_sftp()
-        try:
-            glob_expression: str = ""
-            remote_files = ""
-            if any(char in remote_path for char in ['*', '?', ']', '[']):
-                glob_expression = os.path.basename(remote_path)
-                remote_path = os.path.dirname(remote_path)
-                remote_files = sftp.listdir(remote_path)
-            elif remote_path[-1] == '/':
-                remote_files = sftp.listdir(remote_path)
-            else:
-                remote_files = remote_path
-
-            if not remote_files:
-                module.fail_json(msg=f"No files found matching {remote_path}")
-
-            if (remote_path[-1] == '/' or glob_expression) and local_path[-1] != '/':
-                module.fail_json(msg=f"invalid local_path: {local_path}  -- local_path must be a directory string ending with '/'")
-
-            if type(remote_files) is list:
-                if glob_expression:
-                    remote_files = fnmatch.filter(remote_files, glob_expression)
-                for remote_file in remote_files:
-                    local_file = os.path.join(local_path, remote_file)
-
-                    # Check if file exists locally and compare content
-                    if os.path.exists(local_file):
-                        with open(local_file, 'rb') as f:
-                            local_hash = get_file_hash(f)
-
-                        with sftp.file(remote_file, 'rb') as f:
-                            remote_hash = get_file_hash(f)
-
-                        if local_hash == remote_hash:
-                            continue  # Skip this file, it's already up to date
-
-                    # Download the file
-                    sftp.get(os.path.join(remote_path, remote_file), local_file)
-                    result['changed'] = True
-                    result['downloaded_files'].append(local_file)
-            else:
-                local_file = os.path.join(local_path, remote_files) if local_path[-1] == '/' else local_path
-                sftp.get(remote_path, local_path)
-                result['changed'] = True
-                result['downloaded_files'].append(local_file)
-
-            if result['changed']:
-                result['msg'] = f"{len(result['downloaded_files'])} file(s) downloaded successfully"
-            else:
-                result['msg'] = "All files already exist at destination with the same content"
-
-        except Exception as err:
-            module.fail_json(msg=f'SFTP download failed: {to_native(err)}', **result)
-        finally:
-            sftp.close()
-            ssh.close()
+        module.exit_json(**result)
     except Exception as err:
-        module.fail_json(msg=f'Client error occurred: {to_native(err)}', **result)
-
-    module.exit_json(**result)
+        module.fail_json(msg=f"Error occurred: {to_native(err)}")
 
 
-if __name__ == '__main__':
+def main():
+    spec = dict(
+        host=dict(type="str", required=True),
+        port=dict(default=22, type="int"),
+        username=dict(type="str", required=True),
+        password=dict(type="str", required=True, no_log=True),
+        remote_path=dict(type="str", required=True),
+        local_path=dict(type="str", required=True),
+        host_key_algorithms=dict(type="list", elements="str", required=False),
+    )
+
+    module = AnsibleModule(argument_spec=spec, supports_check_mode=False)
+
+    if module.check_mode:
+        module.exit_json(
+            changed=False,
+            msg="Check mode not supported for file retrieval. Create a PR on the github repo if you want this functionality.",
+        )
+
+    run_module(module)
+
+
+if __name__ == "__main__":
     main()
